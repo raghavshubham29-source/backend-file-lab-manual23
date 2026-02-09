f393b98b4bba4a3bde193f025a1924cfbcdfba63
shubham-kumar <raghavshubham29@gmail.com>
2026-02-09 10:57:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+
+const add = require("./utility");
+
+console.log("Sum:", add(5, 3));
---
+++ utility.js
@@ -0,0 +1,7 @@
+// utility.js
+
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
